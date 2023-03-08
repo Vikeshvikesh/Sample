36512883f400dab0c21ae2b5d3e5199c8630c36e
vikesh <vikesh.balineni@gmail.com>
2023-03-08 19:38:38 +0530

    wednesday

---
+++ src/Wednesday/Single_LinkedList.java
@@ -0,0 +1,36 @@
+package Wednesday;
+
+public class Single_LinkedList {
+    Node head;
+    static class Node {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next=null;
+        }
+    }
+    public void display()
+    {
+        Node n = head;
+        while (n != null)
+        {
+            System.out.print(n.data+" \n");
+            n = n.next;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Single_LinkedList list = new Single_LinkedList();
+
+        list.head       = new Node(100);
+        Node second      = new Node(200);
+        Node third       = new Node(300);
+
+        list.head.next = second;
+        second.next = third;
+        list.display();
+    }
+}
